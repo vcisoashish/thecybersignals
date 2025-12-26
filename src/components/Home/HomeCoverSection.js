67dd96650b569ed93f4d77319d312269d0e8be0c
vcisoashish <vcisoashish@gmail.com>
2025-12-26 18:47:26 +0000

    Fix JSX syntax errors in HomeCoverSection

--- src/components/Home/HomeCoverSection.js
+++ src/components/Home/HomeCoverSection.js
@@ -16,7 +16,7 @@ const HomeCoverSection = ({blogs}) => {
         <article className='flex flex-col items-start justify-end mx-5 sm:mx-10 relative h-[60vh] sm:h-[85vh]'>
             <div className='absolute top-0 left-0 bottom-0 right-0 h-full
             bg-gradient-to-b from-transparent from-0% to-dark/90 rounded-3xl z-0
-            ' />}
+            ' />
         {blog.image && <Image src={blog.image.src}
         placeholder="blur"
         blurDataURL={blog.image.blurDataURL}
@@ -28,7 +28,7 @@ const HomeCoverSection = ({blogs}) => {
         />}
 
         <div className='w-full lg:w-3/4 p-6 sm:p-8 md:p-12  lg:p-16 flex flex-col items-start justify-center z-0 text-light'>
-            <Tag link={`/categories/${slug(blog.tags[0])}`} name={blog.tags[0]} />}
+            <Tag link={`/categories/${slug(blog.tags[0])}`} name={blog.tags[0]} />
             <Link href={getCleanBlogUrl(blog.slug)} className='mt-6'>
             <h1 className='font-bold capitalize text-lg sm:text-xl md:text-3xl lg:text-4xl'>
                 <span className='bg-gradient-to-r from-accent to-accent dark:from-accentDark/50 
